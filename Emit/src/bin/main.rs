73e6ddc0aea2233006b5324c84dcd536be5da699
marek counts <marek.counts@outlook.com>
2018-07-08 22:02:46 -0400

    added clap support and bassic command line structure. some issues with clap on windows but seemed fine on linux.

---
+++ Emit/src/bin/main.rs
@@ -0,0 +1,26 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
+fn main() {
+    let matches = App::new("Emit")
+                        .version("0.1")
+                        .author("Marek Counts <countsmarek@gmail.com>")
+                        .about("Command line untility to assist with using the emit.sh service")
+                        .arg(Arg::with_name("INPUT")
+                            .help("Sets the input file to use")
+                            .required(true)
+                            .index(1))
+                        .arg(Arg::with_name("email")
+                            .required(false)
+                            .help("Ends the emited link to the above email")
+                            .index(2))
+                        .subcommand(SubCommand::with_name("test")
+                                    .about("controls testing features")
+                                    .version("1.3")
+                                    .author("Someone E. <someone_else@other.com>")
+                                    .arg(Arg::with_name("debug")
+                                        .short("d")
+                                        .help("print debug information verbosely")))
+                        .get_matches();
+}
